4b47cc102bf735a05e89855b5ba6fb400bb55a83
Valeriia <valeriia.air@gmail.com>
2022-09-20 16:01:43 +0300

    npm install @reduxjs/toolkit react-redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from 'components/App'
+
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
+
 import { ThemeProvider } from '@emotion/react'
 import { theme } from './theme'
 import './index.css'
@@ -8,7 +12,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+    <Provider store={store}>
       <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 )
