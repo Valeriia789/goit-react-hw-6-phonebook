a197073c1a57efa657d7422b83f1bd56352f116c
Valeriia <valeriia.air@gmail.com>
2022-09-23 00:46:43 +0300

    перевірка імені нового контакту серед списку

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -15,6 +15,14 @@ const filtersInitialState = {
 const contactsReducer = (state = contactsInitialState, action) => {
   switch (action.type) {
     case 'contacts/addContact':
+      const names = state.map(contact => contact.name)
+      console.log(names)
+      console.log(action.payload.name)
+
+      if (names.find(myContact => myContact === action.payload.name)) {
+        alert(`${action.payload.name} is already in contacts`)
+        return state
+      }
       return [...state, action.payload]
 
     case 'contacts/deleteContact':
@@ -46,66 +54,7 @@ const filtersReducer = (state = filtersInitialState, action) => {
   }
 }
 
-// export const rootReducer = (state = {}, action) => {
-//   return {
-//     contacts: contactsReducer(state.contacts, action),
-//     filters: filtersReducer(state.filters, action),
-//   };
-// };
-
 export const rootReducer = combineReducers({
   contacts: contactsReducer,
   filters: filtersReducer
 })
-
-// const initialState = {
-//   contacts: [
-//     { id: 1, name: 'Rosie Simpson', number: '459-12-56', group: true },
-//     { id: 2, name: 'Hermione Kline', number: '443-89-12', group: true },
-//     { id: 3, name: 'Eden Clements', number: '645-17-79', group: false },
-//     { id: 4, name: 'Annie Copeland', number: '227-91-26', group: false }
-//   ],
-//   filters: {
-//     status: statusFilters.all
-//   }
-// }
-
-// export const rootReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case 'contacts/addContact':
-//       return {
-//         ...state,
-//         contacts: [...state.contacts, action.payload]
-//       }
-//     case 'contacts/deleteContact':
-//       return {
-//         ...state,
-//         contacts: state.contacts.filter(
-//           contact => contact.id !== action.payload
-//         )
-//       }
-//     case 'contacts/toggleGroup':
-//       return {
-//         ...state,
-//         contacts: state.contacts.map(contact => {
-//           if (contact.id !== action.payload) {
-//             return contact
-//           }
-//           return {
-//             ...contact,
-//             group: !contact.group
-//           }
-//         })
-//       }
-//     case 'filters/setStatusFilter':
-//       return {
-//         ...state,
-//         filters: {
-//           ...state.filters,
-//           status: action.payload
-//         }
-//       }
-//     default:
-//       return state
-//   }
-// }
