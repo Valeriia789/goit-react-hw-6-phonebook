ec20f483ed8b7c2927d289c60b85e902525370e6
Valeriia <valeriia.air@gmail.com>
2022-09-23 01:18:16 +0300

    createAction

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,34 +1,53 @@
-import { nanoid } from "nanoid";
+import { createAction } from '@reduxjs/toolkit'
+import { nanoid } from 'nanoid'
 
-export const addContact = (name, number) => {
-  return {
-    type: "contacts/addContact",
-    payload: {
-      id: nanoid(),
-      group: false,
-      name,
-      number,
-    },
-  };
-};
+export const addContact = createAction(
+  'contacts/addContact',
+  (name, number) => {
+    return {
+      payload: {
+        id: nanoid(),
+        group: false,
+        name,
+        number
+      }
+    }
+  }
+)
 
-export const deleteContact = contactId => {
-  return {
-    type: "contacts/deleteContact",
-    payload: contactId,
-  };
-};
+export const deleteContact = createAction('contacts/deleteContact')
+export const toggleGroup = createAction('contacts/toggleGroup')
+export const setStatusFilter = createAction('filters/setStatusFilter')
 
-export const toggleGroup = contactId => {
-  return {
-    type: "contacts/toggleGroup",
-    payload: contactId,
-  };
-};
+// export const addContact = (name, number) => {
+//   return {
+//     type: "contacts/addContact",
+//     payload: {
+//       id: nanoid(),
+//       group: false,
+//       name,
+//       number,
+//     },
+//   };
+// };
 
-export const setStatusFilter = value => {
-  return {
-    type: "filters/setStatusFilter",
-    payload: value,
-  };
-};
+// export const deleteContact = contactId => {
+//   return {
+//     type: "contacts/deleteContact",
+//     payload: contactId,
+//   };
+// };
+
+// export const toggleGroup = contactId => {
+//   return {
+//     type: "contacts/toggleGroup",
+//     payload: contactId,
+//   };
+// };
+
+// export const setStatusFilter = value => {
+//   return {
+//     type: "filters/setStatusFilter",
+//     payload: value,
+//   };
+// };
